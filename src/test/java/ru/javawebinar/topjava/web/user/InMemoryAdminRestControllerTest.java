a17e01379b3cde493f448fbef1ed2dc7de965cf7
Ticherti <vera.karr@gmail.com>
2021-10-24 18:01:57 +0300

    HW 3 Fix InMemory..ControllerSpringTest, by adding a solo standing config for tests

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -1,10 +1,13 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
@@ -13,6 +16,8 @@ import java.util.Arrays;
 import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
+@ContextConfiguration("classpath:spring-app-test.xml")
+@RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerTest {
     private static final Logger log = LoggerFactory.getLogger(InMemoryAdminRestControllerTest.class);
 
